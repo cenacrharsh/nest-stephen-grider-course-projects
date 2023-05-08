8600d53b93b017422b35aa87200a07a0aa58e278
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2023-05-08 15:50:15 +0530

    updating session in signup and signin

--- car-value-estimator/src/users/users.controller.ts
+++ car-value-estimator/src/users/users.controller.ts
@@ -27,14 +27,18 @@ export class UsersController {
     ) {}
 
     @Post('/signup')
-    createUser(@Body() body: CreateUserDto) {
+    async createUser(@Body() body: CreateUserDto, @Session() session: any) {
         console.log(body);
-        this.authService.signup(body.email, body.password);
+        const user = await this.authService.signup(body.email, body.password);
+        session.userId = user.id;
+        return user;
     }
 
     @Post('/signin')
-    signin(@Body() body: CreateUserDto) {
-        return this.authService.signin(body.email, body.password);
+    async signin(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.authService.signin(body.email, body.password);
+        session.userId = user.id;
+        return user;
     }
 
     //* id's are numbers, but everything in incoming req comes as a string
